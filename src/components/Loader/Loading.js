d5d742ac713a0d21fcc12a0c82d34729cd34826d
Rahul <rahulverma11199@gmail.com>
2023-02-08 11:54:11 +0530

    Fixing Loading Spinner

--- src/components/Loader/Loading.js
+++ src/components/Loader/Loading.js
@@ -3,7 +3,7 @@ import classes from "./Loading.module.css";
 export default function Loading() {
   return (
     <div className={classes.loadingCont}>
-      <div className={classes.loading}>( o )</div>
+       
     </div>
   );
 }
